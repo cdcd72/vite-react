265e4821c3ac62086a22e6972d7a03192ffecc07
cdcd72 <cdcd71517@gmail.com>
2023-11-20 10:47:35 +0800

    Update tailwindcss demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="flex items-center justify-center">
-      <span className="mt-10 text-3xl">Hello React!</span>
+      <span className="mt-10 text-3xl font-semibold">Hello React!</span>
     </div>
   );
 }
